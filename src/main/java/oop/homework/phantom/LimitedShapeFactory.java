795652ff4d1e33f4af5785bf7d768f1bcaed24bf
Koshlatyi-ML <ml.koshlatyi@gmail.com>
2017-02-22 02:15:22 +0200

    homework finished

--- src/main/java/oop/homework/phantom/LimitedShapeFactory.java
+++ src/main/java/oop/homework/phantom/LimitedShapeFactory.java
@@ -2,28 +2,41 @@ package oop.homework.phantom;
 
 import oop.homework.geometry.Point;
 import oop.homework.geometry.Shape;
+import oop.homework.geometry.Triangle;
+import oop.homework.geometry.factory.Figure;
+import oop.homework.geometry.factory.ShapeFactories;
 import oop.homework.geometry.factory.ShapeFactory;
 
 import java.lang.ref.PhantomReference;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
+import java.lang.reflect.Field;
 
 public class LimitedShapeFactory {
     private int limit;
     private volatile int count;
 
     private ReferenceQueue<Shape> referenceQueue = new ReferenceQueue<>();
-    private Shape strongRef = null;
+
+//    private Thread cleaner = new Thread(() -> {
+//        while (true) {
+//            if (referenceQueue.poll() != null) {
+//                count--;
+//            }
+//        }
+//    });
 
     public LimitedShapeFactory(int limit) {
         this.limit = limit;
-
-        new Thread(() -> {
-            while (true) {
-                strongRef = null;
-            }
-        }).start();
     }
+
+//    private void startCleaner() {
+//        cleaner.start();
+//    }
+//
+//    private void stopCleaner() {
+//        cleaner.interrupt();
+//    }
 
     public Shape createObject(ShapeFactory factory, Point[] points) {
         while (count >= limit) {
@@ -31,6 +44,7 @@ public class LimitedShapeFactory {
         }
 
         Reference<? extends Shape> phantomRef;
+        Shape strongRef;
         synchronized (this) {
             strongRef = factory.createShape(points);
             phantomRef = new PhantomReference<>(strongRef, referenceQueue);
@@ -56,5 +70,54 @@ public class LimitedShapeFactory {
 
         phantomRef.clear();
         count--;
+    }
+
+    public long getCount() {
+        long result = -1;
+
+        try {
+            Field queueLength = referenceQueue.getClass()
+                                              .getDeclaredField("queueLength");
+            queueLength.setAccessible(true);
+            result =  queueLength.getLong(referenceQueue);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LimitedShapeFactory factory = new LimitedShapeFactory(5);
+
+        ShapeFactory shapeFactory = ShapeFactories.getFactory(Figure.RING);
+
+        System.out.println(factory.getCount());
+
+        factory.createObject(shapeFactory, new Point[]{new Point(1, 2),
+                                                       new Point(3, 4)});
+        System.out.println(factory.getCount());
+        factory.createObject(shapeFactory, new Point[]{new Point(11, 2),
+                new Point(3, 4)});
+        System.out.println(factory.getCount());
+        factory.createObject(shapeFactory, new Point[]{new Point(1, 21),
+                new Point(3, 4)});
+        System.out.println(factory.getCount());
+        factory.createObject(shapeFactory, new Point[]{new Point(1, 2),
+                new Point(3, 14)});
+        System.out.println(factory.getCount());
+        factory.createObject(shapeFactory, new Point[]{new Point(11, 21),
+                new Point(3, 4)});
+        System.out.println(factory.getCount());
+        factory.createObject(shapeFactory, new Point[]{new Point(12, 2),
+                new Point(3, 14)});
+        System.out.println(factory.getCount());
+        factory.createObject(shapeFactory, new Point[]{new Point(12, 2),
+                new Point(3, 14)});
+        System.out.println(factory.getCount());
+
+        factory = null;
     }
 }
