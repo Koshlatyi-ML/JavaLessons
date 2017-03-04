dd12a157bf8d2c428ea7e174c1e5bed8bad98ce1
Koshlatyi-ML <ml.koshlatyi@gmail.com>
2017-03-04 21:27:26 +0200

    wiki protype pattern

--- src/main/java/oop/homework/patterns/abstr_factory/MovieRental.java
+++ src/main/java/oop/homework/patterns/abstr_factory/MovieRental.java
@@ -1,11 +1,25 @@
 package oop.homework.patterns.abstr_factory;
 
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class MovieRental {
-    private static Map<Movie, List<Language>> moviesCatalog;
+    private static Map<Movie, List<Language>> moviesCatalog
+            = new HashMap<Movie, List<Language>> () {{
+                put(new Movie("Interstellar"), new ArrayList<Language>() {{
+                    add(Language.ENGLISH);
+                    add(Language.FRENCH);
+                    add(Language.RUSSIAN);
+                }});
+                put(new Movie("Trainspotting"), new ArrayList<Language>() {{
+                    add(Language.ENGLISH);
+                    add(Language.NORWEGIAN);
+                }});
+                put(new Movie("Star Wars 3"), new ArrayList<Language>() {{
+                    add(Language.ENGLISH);
+                    add(Language.NORWEGIAN);
+                    add(Language.UKRAINIAN);
+                }});
+    }};
 
     public static Movie rentMovie(String title, Language language) {
         Movie movie = moviesCatalog.keySet().stream()
